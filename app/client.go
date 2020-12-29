0b5ec42ea5151d5abcc89e22f6b23b538d0c9802
ralpioxxcs <tmfmfkal1123@naver.com>
2020-12-29 23:36:39 +0900

    layout

--- app/client.go
+++ app/client.go
@@ -2,16 +2,13 @@ package app
 
 import (
 	"fmt"
-	c "github.com/jroimartin/gocui"
 	"io/ioutil"
 	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
-const (
-	lw = 20
-	ih = 3
-)
-
+// Run app
 func Run() {
 	g, err := initailize()
 	if err != nil {
@@ -23,66 +20,84 @@ func Run() {
 	g.MainLoop()
 }
 
-func initailize() (*c.Gui, error) {
-	g, err := c.NewGui(c.OutputNormal)
+// initailize cui interface
+func initailize() (*gocui.Gui, error) {
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		return g, err
 	}
 
 	g.Cursor = true
-
 	g.SetManagerFunc(layout)
 
-	if err = initKeybinding(g); err != nil {
+	if err = keybinding(g); err != nil {
 		return g, err
 	}
 	return g, nil
 }
 
-func layout(g *c.Gui) error {
-	tw, th := g.Size()
+func layout(g *gocui.Gui) error {
 
-	maxX, maxY := tw-1, th-1
+	/*
+			    [ Layout ]
 
-	// Typing window
-	viewport, err := g.SetView("words", 0, 0, maxX, maxY/3)
-	if err != nil && err != c.ErrUnknownView {
+			-----------------
+			 t    typing
+			 h
+			 e ______________
+			 m
+		 	 e 	 live wpm
+			_________________
+	*/
+
+	maxX, maxY := g.Size()
+
+	// pane 1 - theme
+	theme, err := g.SetView("theme", -1, -1, 30, maxY)
+	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
-	viewport.Title = "Typing Window"
-	//viewport.BgColor = c.ColorGreen
-	viewport.FgColor = c.ColorWhite
-	viewport.Autoscroll = true
+	theme.Highlight = true
+	theme.SelBgColor = gocui.ColorGreen
+	theme.SelFgColor = gocui.ColorBlack
+	fmt.Fprintln(theme, "8008")
+	fmt.Fprintln(theme, "Dracula")
 
-	// paragraphs init
+	// pane 2.1 - sentence viewport
+	sviewport, err := g.SetView("sentence", 30, 0, maxX, maxY)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+	sviewport.Title = "viewport"
 	b, err := ioutil.ReadFile("../data/charlieandchcolatefactory.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(viewport, "%s", b)
+	fmt.Fprintf(sviewport, "%s", b)
 
-	// 2. Input
-	_, h := viewport.Size()
-	input, err := g.SetView("input", 0, h/2, tw-1, h+1)
-	if err != nil && err != c.ErrUnknownView {
+	// pane 2.2 - typing viewport
+	_, h := sviewport.Size()
+	tviewport, err := g.SetView("typing", 30, h/2, maxX, maxY)
+	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
-	input.Frame = true
-	input.Title = "types here"
-	//input.BgColor = c.ColorBlack
-	input.FgColor = c.ColorYellow
-	input.Editable = true
-	input.Wrap = true
-	if _, err = g.SetCurrentView("input"); err != nil {
+	tviewport.Frame = true
+	tviewport.Title = "types here"
+	tviewport.FgColor = gocui.ColorYellow
+	tviewport.Editable = true
+	tviewport.Wrap = true
+
+	if _, err = g.SetCurrentView("typing"); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func initKeybinding(g *c.Gui) error {
-	g.SetKeybinding("", c.KeyCtrlC, c.ModNone, func(g *c.Gui, v *c.View) error {
-		return c.ErrQuit
+// keybinding configurate key bindings each pane
+func keybinding(g *gocui.Gui) error {
+	g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		return gocui.ErrQuit
 	})
 	return nil
 }
---
+++ app/wpm.go
@@ -0,0 +1,9 @@
+package app
+
+import (
+	"fmt"
+)
+
+func Test() {
+	fmt.Println("wpm calcu")
+}
