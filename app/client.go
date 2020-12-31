dd97892e29ef19063fa81454b1fff1f1ebde3cd0
ralpioxxcs <tmfmfkal1123@naver.com>
2020-12-31 20:50:13 +0900

    apply primitive type, box

--- app/client.go
+++ app/client.go
@@ -6,32 +6,38 @@ import (
 )
 
 func Run() {
+
+	// primitive type based on text view
 	newPrimitive := func(text string) tview.Primitive {
 		return tview.NewTextView().
 			SetTextAlign(tview.AlignCenter).
-			SetText(text)
-		//SetBackgroundColor(tcell.ColorRoyalBlue)
+			SetText("").
+			SetTextColor(tcell.ColorBlue).
+			SetBackgroundColor(tcell.ColorWheat)
 	}
-	palette := newPrimitive("Palette")
+
 	main := newPrimitive("Main")
+	box := tview.NewBox().
+		SetBorder(true).
+		SetBorderAttributes(tcell.AttrBold).
+		SetTitle("Palette").
+		SetBackgroundColor(tcell.ColorWheat)
 
 	grid := tview.NewGrid().
 		SetRows(1).
-		SetColumns(15, 0).
-		SetBorders(true).
+		SetColumns(20, 0).
+		SetBorders(false).
 		SetBordersColor(tcell.ColorBlueViolet)
-
-	grid.SetTitleColor(tcell.ColorGold)
 
 	//grid.SetBackgroundColor(tview.Styles.PrimitiveBackgroundColor)
 	//grid.SetBackgroundColor(tview.Styles.ContrastBackgroundColor)
 
 	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-	grid.AddItem(palette, 0, 0, 0, 0, 0, 0, false).
+	grid.AddItem(box, 0, 0, 0, 0, 0, 0, false).
 		AddItem(main, 1, 0, 1, 3, 0, 0, false)
 
 	// Layout for screens wider than 100 cells.
-	grid.AddItem(palette, 1, 0, 1, 1, 0, 100, false).
+	grid.AddItem(box, 1, 0, 1, 1, 0, 100, false).
 		AddItem(main, 1, 1, 1, 1, 0, 100, false)
 
 	if err := tview.NewApplication().SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
